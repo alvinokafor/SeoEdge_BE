8154d7150a13ffd4f83601b2c5b692521f4c1cca
Alvin Okafor <alvin2k99@gmail.com>
2025-05-29 21:07:58 +0100

    update

--- dist/modules/seo/seo.service.js
+++ dist/modules/seo/seo.service.js
@@ -336,7 +336,7 @@ class SeoService {
                         '--disable-dev-shm-usage',
                     ],
                     // <-- tell chrome-launcher where to find chromium
-                    chromePath: process.env.CHROMIUM_PATH || '/usr/bin/chromium',
+                    // chromePath: process.env.CHROMIUM_PATH || '/usr/bin/chromium',
                 });
                 options = {
                     logLevel: 'verbose',
